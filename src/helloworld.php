96f8777c57495a18e403d5b0d3fe9c7595f7eaf4
Patrick Baber <patrick@p-squared.de>
2019-05-14 10:05:17 +0200

    add some easy php examples

---
+++ src/helloworld.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'Hello world!!!\n';
+echo "Hello world\t!!!\n";
---
+++ src/htmlandphp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1><?php echo 'Title'; ?></h1>
+        <p><?php echo 'Paragraph'; ?></p>
+        <?php echo '<p>Paragraph with PHP</p>'; ?>
+    </body>
+</html>
